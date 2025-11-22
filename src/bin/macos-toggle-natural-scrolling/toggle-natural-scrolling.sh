d5c74b0af5a4293add82a7003a11bac78909bfef
Allovelle <167846019+allovelle@users.noreply.github.com>
2025-11-21 22:41:19 -0500

    fixed broken binary path for toggle natural scrolling

--- src/bin/macos-toggle-natural-scrolling/toggle-natural-scrolling.sh
+++ src/bin/macos-toggle-natural-scrolling/toggle-natural-scrolling.sh
@@ -4,4 +4,4 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 echo ':mushroom:'  # More Emojis: https://gist.github.com/rxaviers/7360908
 echo '---'
-echo "Toggle Natural Scrolling | shell='$(which macos-natural-scrolling)'"
+echo "Toggle Natural Scrolling | shell='$(which macos-toggle-natural-scrolling)'"
